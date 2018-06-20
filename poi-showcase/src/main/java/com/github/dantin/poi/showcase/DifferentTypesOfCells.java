7f7f0372aaf5ce47781eb9fe9d180e61f90f0b21
dantin <chengjie.ding@gmail.com>
2018-06-20 13:57:01 +0800

    Working with different type of cells.

---
+++ poi-showcase/src/main/java/com/github/dantin/poi/showcase/DifferentTypesOfCells.java
@@ -0,0 +1,38 @@
+package com.github.dantin.poi.showcase;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Working with different types of cells.
+ *
+ * @version 1.0 2018-06-20
+ */
+public class DifferentTypesOfCells {
+    public static void main(String[] args) throws IOException {
+        Workbook wb = new XSSFWorkbook();  // or new HSSFWorkbook();
+
+        Sheet sheet = wb.createSheet("new sheet");
+        Row row = sheet.createRow(2);
+        row.createCell(0).setCellValue(1.1);
+        row.createCell(1).setCellValue(new Date());
+        row.createCell(2).setCellValue(Calendar.getInstance());
+        row.createCell(3).setCellValue("a string");
+        row.createCell(4).setCellValue(true);
+        row.createCell(5).setCellType(CellType.ERROR);
+
+        // Write the output to a file
+        try (OutputStream fileOut = new FileOutputStream("workbook.xlsx")) {
+            wb.write(fileOut);
+        }
+    }
+}
